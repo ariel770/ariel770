3c7c53bb61285a0a5226943ed0150c472eb2c749
Ariel Rosemberg <ariel.rosemberg@innovid.com>
2021-02-23 15:01:09 +0200

    create new account class

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+     String name ;
+        Account account =new Account();
+       account.setName("Pinjas Rosenberg");
+       name = account.getName();
+       System.out.println(name);
+    }
+}
