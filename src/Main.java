f03740ce25c61d8aac875247764f3c15b4d02806
Ariel Rosemberg <ariel.rosemberg@innovid.com>
2021-02-23 15:23:47 +0200

    create name account  from main

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
      String name ;
+       //create an object and assign it to my account
         Account account =new Account();
-       account.setName("Pinjas Rosenberg");
-       name = account.getName();
-       System.out.println(name);
+        System.out.printf("Initial name is :  %s%n%n ",account.getName());
+        System.out.println("Enter your name please !");
+        name = scanner.nextLine();
+        account.setName(name);
+        System.out.printf("The name is :  %n%s%n ",account.getName());
     }
 }
