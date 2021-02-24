69b3ff90621cb52cd564532acb5ac0ac1747ffb5
Ariel Rosemberg <ariel.rosemberg@innovid.com>
2021-02-24 14:38:26 +0200

    Calculating a class average  and sentinel controler

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,17 @@ public class Main {
 
         //calculate average
         int total = 0 ;
-        int students = 1 ;
+        int students = 0 ;
         int grade = 0;
 
-        System.out.println("Enter a result of test ! ");
+        System.out.println("Enter a results of test ! ");
         Scanner scanner = new Scanner(System.in);
-        while(students <= 10){
+        while(grade != -1){
             grade = scanner.nextInt();
             total += grade ;
             students ++;
             System.out.println("The student #"+students +" has a "+ grade);
+            System.out.println(" If you want to exit press -1  !");
         }
         int average = total/students;
         System.out.println("The sum is  : "+ total + " for "+students+" students ,  so the avg is :  "+ average );
