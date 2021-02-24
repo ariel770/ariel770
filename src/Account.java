c9aae6d50d3b1bbe67ccd9ce61eb4b9d973f04ab
Ariel Rosemberg <ariel.rosemberg@innovid.com>
2021-02-24 09:44:35 +0200

    Using the account ctr to initialize the name IV when is created

--- src/Account.java
+++ src/Account.java
@@ -1,9 +1,21 @@
 public class Account {
-    private String name ;
+
+    private String name ;//instance variable
+//     contractor with parameters
+    public Account(String name){
+       this.name=name;
+
+    }
+
+
+
+    //method that set the name of  instance variable
     public void setName(String name){
         this.name=name;
     }
+    //method that get the name from the instance variable
     public String getName(){
+        
         return this.name;
     }
 
--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,15 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-     String name ;
-       //create an object and assign it to my account
-        Account account =new Account();
-        System.out.printf("Initial name is :  %s%n%n ",account.getName());
-        System.out.println("Enter your name please !");
-        name = scanner.nextLine();
-        account.setName(name);
-        System.out.printf("The name is :  %n%s%n ",account.getName());
+
+            //create an object and assign it to my account
+
+        Account account =new Account("ariel rosemberg");
+        Account account1 =new Account("pinjas rosemberg");
+
+
+        System.out.printf("The first name  :  %n%s%n ",account.getName());
+
+        System.out.printf("The second name is :  %n%s%n ",account1.getName());
     }
 }
