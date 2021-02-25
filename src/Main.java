962b632a10f4b83a4a5a93e31ece68259f431e63
Ariel Rosemberg <ariel.rosemberg@innovid.com>
2021-02-25 12:22:50 +0200

    break and continue statements

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,22 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        AutoPolicy autoPolicy1 = new AutoPolicy("Toyota Camery",2020);
-        AutoPolicy autoPolicy2 = new AutoPolicy("Nissan  Altima",2010);
-        getTheAutoPolicyOfCars(autoPolicy1);
-        getTheAutoPolicyOfCars(autoPolicy2);
-    }
-    public static void getTheAutoPolicyOfCars(AutoPolicy autoPolicy ){
-        System.out.println(autoPolicy.getCompanyName());
-        System.out.println(autoPolicy.getModel());
-        System.out.println(autoPolicy.getFullCover());
+        // break - continue statement
+
+        int counter ;
+        for (counter = 0 ;counter<= 10 ;counter++){
+            if(counter==5)break;
+            System.out.println(counter);
+
+        }
+        System.out.println("the iteration has stopped at number : " + counter);
+        for (int counter1 = 0 ;counter1<=10 ;counter1++){
+            if(counter1==5)continue;
+            System.out.println(counter1);
+
+        }
+        System.out.println("the iteration has skiped the 5 number");
+
 
     }
 }
