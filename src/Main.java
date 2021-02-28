ef1672b62a178057bd54139fbe4b70eb0918f1be
Ariel Rosemberg <ariel.rosemberg@innovid.com>
2021-02-28 17:47:23 +0200

    Shifted and scaled random integers

--- src/Main.java
+++ src/Main.java
@@ -5,21 +5,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Enter double numbers to test which is the higher  (separate by Enter ) : ");
-        Scanner scanner = new Scanner(System.in);
-        double one = scanner.nextDouble();
-        double tow = scanner.nextDouble();
-        double three = scanner.nextDouble();
-        double max =  maximum(one,tow,three);
-        System.out.println("The high number is : "+ max);
-    }
-    public static double maximum(double one ,double tow ,double three ) {
-        double maxValue = one;
-        if(tow > maxValue){
-            maxValue = tow;
-        }if (three  > maxValue){
-            maxValue = three;
+        SecureRandom sr = new SecureRandom();
+        for(int counter =1 ; counter<= 20 ;counter++){
+            System.out.print(" "+sr.nextInt(6));
+            if(counter % 5 == 0 ){
+                System.out.println();
+
+            }
+
+
         }
-        return maxValue ;
-    }
+     }
+
 }
