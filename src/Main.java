4f67e198fc89ffa7f8c65ffa5e733d40919f11d7
Ariel Rosemberg <ariel.rosemberg@innovid.com>
2021-03-02 16:50:34 +0200

    Poll analysis program (try catch +array exeptions )

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,20 @@ import java.security.SecureRandom;
 
 public class Main {
     public static void main(String[] args) {
-        int arr[] = new int[7];
-        SecureRandom sr = new SecureRandom();
-        for (int count = 0; count <= 6000000; count++) {
-            ++arr[1 + sr.nextInt(6)];
+        int responses[] = {1, 3, 4, 5, 2, 1, 3, 4, 5, 4, 4, 13};
+        int frequency[] = new int[6];
+        for (int count = 0; count < responses.length; count++) {
+            try {
+                ++frequency[responses[count]];
+            }catch (ArrayIndexOutOfBoundsException e){
+                System.out.println(e);
+            }
+
         }
-        for (int count = 1; count < arr.length; count++) {
-            System.out.println(arr[count]);
+        for (int count = 1; count < frequency.length; count++) {
+            System.out.println(frequency[count]);
         }
+
 
     }
 
