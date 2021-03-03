b9aa146ca9d7f0b87f7d419e971709a3159717dc
Ariel Rosemberg <ariel.rosemberg@innovid.com>
2021-03-03 11:32:10 +0200

    By refernce & and By value

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,25 @@ import java.security.SecureRandom;
 public class Main {
     public static void main(String[] args) {
 
-        int [] arr = {12,32,43,5,45,65,656,766,76,67,45,23};
-        int total =0;
-       for(int count : arr){
-          total+= count;
-       }
+        int  [] arr = {12,32,43,5,45,65,656,766,76,67,45,23};
+        int total = 5;
 
         System.out.println(total);
+        System.out.println(arr[0]);
+        changeValue(arr);
+        changeValueInt(total);
+
+        System.out.println(total);
+        System.out.println(arr[0]);
+
     }
+    public static void changeValue( int [] arr){
+       arr[0]=9;
+    }
+    public static void changeValueInt( int total){
+        total=7;
+    }
+
 
 }
 
