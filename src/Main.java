c2987ffe155da9f0a60f0363c5729324ca15d966
Ariel Rosemberg <ariel.rosemberg@innovid.com>
2021-03-03 15:18:34 +0200

    Generic ArrayList<T> collection demonstration

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,19 @@
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        double arr[] = {2.2, 3.2, 56.6, 7.3};
-        displayarray(arr);
-        Arrays.sort(arr);
-        displayarray(arr);
-        Arrays.fill(arr , 10);
-        displayarray(arr);
+        ArrayList <String> arrayList = new ArrayList();
+        arrayList.add("yellow");
+        arrayList.add(0,"red");
+        displayArrayList(arrayList);
+
 
     }
-    public static void displayarray(double [] arr){
-        for(int count = 0 ; count<arr.length ;count++)
-        {
-            System.out.print(arr[count] +"  ");
+    public static void displayArrayList(ArrayList arrayList){
+        for(int counter =0 ;counter< arrayList.size();counter++){
+            System.out.print(arrayList.get(counter) +" ");
         }
-        System.out.println();
     }
 }
