a9bd8e08deb4dce502b7715a3ce1bcfd07dd5817
Ariel Rosemberg <ariel.rosemberg@innovid.com>
2021-03-03 14:11:52 +0200

    Using variable-length argument lists

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,20 @@ import java.security.SecureRandom;
 
 public class Main {
     public static void main(String[] args) {
+     double a =10 ;
+     double b =20;
+     double c =30;
+     double d =40;
+     double e =50;
+     addDoubles(a,b,e);
 
-        int[][] gradesBook ={
-                {12, 32, 43},
-                {5, 45, 65},
-                {56, 96, 76},
-                {67, 45, 23}};
-      GradeBook gradeBook = new GradeBook(gradesBook,"JAVA 2D Arrays");
-      gradeBook.getResults();
     }
-
+    public static double addDoubles(double... doubles){
+         double s = 0 ;
+        for(int count =0 ; count < doubles.length;count++  ){
+            s += doubles[count];
+        }
+       System.out.println(s);
+        return s;
+    }
 }
