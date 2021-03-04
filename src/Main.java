9ad7b401be8ef3495291baf1d2ae8c3d884702da
Ariel Rosemberg <ariel.rosemberg@innovid.com>
2021-03-04 17:17:13 +0200

    explicitly to refer to members of an object

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,27 @@
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        Time1 time1 = new Time1();
-        System.out.println("Initialize time in this parameters : ");
-        System.out.println(time1.toString());
-        System.out.println(time1.formatedDateTime());
-        time1.setTime(11, 34, 34);
-        System.out.println("After initializing time in this parameters : ");
-        System.out.println(time1.toString());
-        System.out.println(time1.formatedDateTime());
-        try {
-            time1.setTime(67, 67, 56);
-        }catch (Exception e) {
-              System.out.println(e.getMessage());
-        }
-        System.out.println("After initializing time in wrong parameters : ");
-        System.out.println(time1.toString());
-        System.out.println(time1.formatedDateTime());
-        time1.setTime(15, 34, 22);
-        System.out.println("After initializing time in this parameters : ");
-        System.out.println(time1.toString());
-        System.out.println(time1.formatedDateTime());
+        DateTime dt = new DateTime(03,23,3);
+       System.out.println(dt.formatedDateTime());
+    }
+}
+class DateTime{
+    private int hour;
+    private int minute;
+    private int seconds;
 
+    public DateTime(int hour, int minute, int seconds){
+        this.hour=hour;
+        this.minute=minute;
+        this.seconds= seconds;
+    }
 
+    public String toString() {
+        return String.format("%02d:%02d:%02d ", hour, minute, seconds);
+    }
+
+    public String formatedDateTime() {
+        return  "This is function that is calling in implicit : "+ toString()+" And this is function that is calling in explicit :  "+this.toString();
     }
 }
