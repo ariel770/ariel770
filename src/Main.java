e94d7c63fe4ba0c6b4c604dc99df98d9ad3d42a0
Ariel Rosemberg <ariel.rosemberg@innovid.com>
2021-03-07 14:29:45 +0200

    class are accessible by other classes in the same package

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-      for (Book book : Book.values()){
-          System.out.println(book.getLanguage()+" : "+book.getYear());
+        AnotherClass ac1 = new AnotherClass();
+        System.out.printf("%n%s", ac1.toString());
+        ac1.age = 30;
+        ac1.name = "pinjas";
+        System.out.printf("%n%s", ac1.toString());
 
-      }
+    }
+}
+
+
+class AnotherClass {
+    int age = 0;
+    String name = "hello";
+
+    public String toString() {
+        return String.format("%n%s%n%s", name, age);
     }
 }
