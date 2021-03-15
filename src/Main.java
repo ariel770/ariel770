9d45ada600f37b9b97b588ca82c59422a32159d7
Ariel Rosemberg <ariel.rosemberg@innovid.com>
2021-03-15 11:56:52 +0200

    try catch finnaly

--- src/Main.java
+++ src/Main.java
@@ -2,32 +2,27 @@ import java.util.Scanner;
 
 public class Main extends Object {
     public static void main(String[] args) {
+        try {
+            throwExeptions();
 
-
-        Scanner scanner = new Scanner(System.in);
-       boolean conitnueLoop = true;
-        do {
-            try {
-            System.out.print("Enter numerator : ");
-            int a = scanner.nextInt();
-
-            System.out.print("Enter denomirator : ");
-            int b = scanner.nextInt();
-            int result = sum(a, b);
-            conitnueLoop = false;
-            System.out.println(result);
-            } catch (Exception e){
-                scanner.nextLine();
-                System.out.println(e.getMessage());
-            }
-        } while (conitnueLoop);
-
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("throw for exeptions");
+        }
 
     }
 
-    public static int sum(int numerator, int denominator) {
+    public static void throwExeptions() throws Exception {
 
-        return numerator / denominator;
+        try {
+            throw new Exception();
+
+        } catch (Exception e) {
+            System.out.println("throw second exeptions");
+            System.out.println(e.getMessage());
+        } finally {
+            throw new Exception();
+        }
     }
 }
 
