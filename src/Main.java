3435b66353d29bc7ff95cdbc9a28c703cbc74069
Ariel Rosemberg <ariel.rosemberg@innovid.com>
2021-03-15 18:02:31 +0200

    Writing data to a sequential text file with class Formatter

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,57 @@
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.file.DirectoryStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Dictionary;
+import java.util.Formatter;
+import java.util.FormatterClosedException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Main extends Object {
+    private static Formatter output;
+
     public static void main(String[] args) throws IOException {
-        System.out.println("Enter Path  :");
-        Scanner input = new Scanner(System.in);
+        openFile();
+        addRecords();
+        closeFile();
+    }
 
-        Path path = Paths.get(input.nextLine());
-        System.out.println("File System p: "+ path.getFileSystem());
-        System.out.println("File Name  p: "+ path.getFileName());
-        System.out.println("File get Root p: "+ path.getRoot());
-        System.out.println("File directory f: "+ Files.isDirectory(path));
-        System.out.println("File lastModify f: "+ Files.getLastModifiedTime(path));
-        if(Files.isDirectory(path)){
-            DirectoryStream<Path> ds = Files.newDirectoryStream(path);
-            for(Path p :ds){
-                System.out.println(p);
-
-            }
+    public static void openFile() {
+        try {
+            output = new Formatter("./src/text.txt");
+        } catch (SecurityException e) {
+            System.out.println("Program is terminating " + e.getMessage());
+            System.exit(1);
+        } catch (FileNotFoundException e) {
+            System.out.println("Program is terminating " + e.getMessage());
+            System.exit(1);
         }
+
+    }
+
+    public static void addRecords() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter please number , firstname ,lastname ,deposit : ");
+        while (scanner.hasNext()) {
+            try {
+                output.format("%d %s %s %.2f%n", scanner.nextInt(), scanner.next(),
+                        scanner.next(), scanner.nextDouble());
+            } catch (FormatterClosedException e) {
+                System.out.println(e.getMessage());
+                break;
+            } catch (NoSuchElementException e) {
+                System.out.println("NoSuchElementException , Please try again :");
+                scanner.nextLine();
+            }
+            System.out.print("?");
+        }
+
+    }
+
+    public static void closeFile() {
+        if (output != null) {
+            output.close();
+        }
+
+
     }
 }
 
