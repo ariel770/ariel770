3b22aebf162d6e50d29bc87939d3c71f57b7aa9a
Ariel Rosemberg <ariel.rosemberg@innovid.com>
2021-03-15 12:30:15 +0200

    Stack unwinding and obtaining data from an exception object

--- src/Main.java
+++ src/Main.java
@@ -2,27 +2,31 @@ import java.util.Scanner;
 
 public class Main extends Object {
     public static void main(String[] args) {
-        try {
-            throwExeptions();
+      try{
+          method1();
 
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            System.out.println("throw for exeptions");
-        }
+      }catch(Exception e){
 
+          System.out.println(e.getMessage());
+          e.printStackTrace();
+          StackTraceElement [] exception = e.getStackTrace();
+
+          for(StackTraceElement ste :exception){
+              System.out.println(ste);
+          }
+
+
+      }
+    }
+    public  static void method1() throws Exception {
+        method2();
+    }
+    public  static void method2() throws Exception {
+        method3();
+    }
+    public  static void method3() throws Exception {
+        throw new Exception("Exeption in method 3");
     }
 
-    public static void throwExeptions() throws Exception {
-
-        try {
-            throw new Exception();
-
-        } catch (Exception e) {
-            System.out.println("throw second exeptions");
-            System.out.println(e.getMessage());
-        } finally {
-            throw new Exception();
-        }
-    }
 }
 
