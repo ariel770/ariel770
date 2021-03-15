59a9d834ab6ac58371c264cf42ca6802aa899787
Ariel Rosemberg <ariel.rosemberg@innovid.com>
2021-03-15 13:58:25 +0200

    chained exeptions

--- src/Main.java
+++ src/Main.java
@@ -2,29 +2,33 @@ import java.util.Scanner;
 
 public class Main extends Object {
     public static void main(String[] args) {
-      try{
-          method1();
+        try {
+            method1();
 
-      }catch(Exception e){
+        } catch (Exception e) {
+            e.printStackTrace();
 
-          System.out.println(e.getMessage());
-          e.printStackTrace();
-          StackTraceElement [] exception = e.getStackTrace();
-
-          for(StackTraceElement ste :exception){
-              System.out.println(ste);
-          }
-
-
-      }
+        }
     }
-    public  static void method1() throws Exception {
-        method2();
+
+    public static void method1() throws Exception {
+        try {
+            method2();
+        } catch (Exception e) {
+            throw new Exception("exeption in method 1",e);
+        }
     }
-    public  static void method2() throws Exception {
-        method3();
+
+    public static void method2() throws Exception {
+        try {
+            method3();
+        } catch (Exception e) {
+            throw new Exception("exeption in method 2",e);
+        }
     }
-    public  static void method3() throws Exception {
+
+    public static void method3() throws Exception {
+
         throw new Exception("Exeption in method 3");
     }
 
