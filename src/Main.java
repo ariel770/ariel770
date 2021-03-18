f3ae8204278916462ddde7bde1e2d376f6536f10
Ariel Rosemberg <ariel.rosemberg@innovid.com>
2021-03-18 17:22:26 +0200

    reading  file from app

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,75 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Paths;
-import java.util.Formatter;
-import java.util.FormatterClosedException;
-import java.util.NoSuchElementException;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main extends Object {
+
+        private static MenuOption[] choices = MenuOption.values();
     private static Scanner input;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         openFile();
-        readRecords();
+        readFile();
         closeFile();
+
     }
+
+//    public static void openFile() {
+//        try {
+//            output = new Formatter("./src/text.txt");
+//        } catch (FileNotFoundException e) {
+//            e.getMessage();
+//            System.exit(1);
+//
+//        }catch(SecurityException e) {
+//            e.getMessage();
+//            System.exit(1);
+//        }
+//
+//    }
+//
+//    public static void addRecords() {
+//        Scanner scanner = new Scanner(System.in);
+//        while (scanner.hasNext()) {
+//
+//            try {
+//                output.format("%d %s %s %d%n", scanner.nextInt(), scanner.next(), scanner.next(), scanner.nextInt());
+//
+//            } catch (FormatterClosedException formatterClosedException){
+//                System.out.println("ERROR 1");
+//                break;
+//            }catch (NoSuchElementException noSuchElementException){
+//                System.out.println("ERROR 2");
+//                break;
+//            }
+//            System.out.print("? ");
+//
+//        }
+//
+//    }
+//
+//    public static void closeFile() {
+//        if (output != null) {
+//            output.close();
+//        }
+
+//    }
 
     public static void openFile() {
         try {
             input = new Scanner(Paths.get("./src/text.txt"));
         } catch (IOException e) {
-            System.out.println("Program is terminating " + e.getMessage());
-            System.exit(1);
+            e.printStackTrace();
         }
 
     }
 
-    public static void readRecords() {
+    public static void readFile() {
         while (input.hasNext()) {
-            try {
-                System.out.printf("%d %s %s %.2f%n", input.nextInt(), input.next(),
-                        input.next(), input.nextDouble());
-            } catch (FormatterClosedException e) {
-                System.out.println(e.getMessage());
-                break;
-            } catch (NoSuchElementException e) {
-                System.out.println("NoSuchElementException , Please try again :");
-                input.nextLine();
-            }
+            System.out.printf("%d %s %s %d%n", input.nextInt(),
+                    input.next(), input.next(), input.nextInt());
+
         }
 
     }
@@ -44,8 +77,8 @@ public class Main extends Object {
     public static void closeFile() {
         if (input != null) {
             input.close();
-        }
 
+        }
 
     }
 }
