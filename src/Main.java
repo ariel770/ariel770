d0c86e585122a4e497dbad508f248823a1045ce2
Ariel Rosemberg <ariel.rosemberg@innovid.com>
2021-03-21 10:28:52 +0200

    input stream

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,54 @@
+import java.io.EOFException;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Main extends Object {
-private static  ObjectOutputStream output ;
+    private static ObjectInputStream input;
+
     public static void main(String[] args) throws IOException {
-     openFile();
-     writeFile();
-     closeFile();
+        openFile();
+        readFile();
+        closeFile();
 
     }
 
-    public static void  openFile(){
+    public static void openFile() {
         try {
-            output = new ObjectOutputStream(Files.newOutputStream(Paths.get("./src/text.txt")));
+            input = new ObjectInputStream(Files.newInputStream(Paths.get("./src/text.txt")));
         } catch (IOException e) {
             e.printStackTrace();
-            System.out.println("error to create output stream");
+            System.out.println("error to create input stream");
         }
 
     }
 
-    public static void writeFile() {
-        Scanner scanner = new Scanner(System.in);
-        while (scanner.hasNext()){
+    public static void readFile() {
 
-            AccountToSerializable records  = new AccountToSerializable(scanner.nextInt(),
-                    scanner.next(),scanner.next(),scanner.nextInt());
-            try {
-                output.writeObject(records);
-            } catch (IOException e) {
-                e.printStackTrace();
+        try {
+            while (true) {
+
+                AccountToSerializable records = (AccountToSerializable) input.readObject();
+                System.out.printf("%s %s %s %s %n",records.getAccountNumber(), records.getFirstName(), records.getLastName(), records.getBalance());
             }
+        } catch (EOFException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
     }
 
     public static void closeFile() throws IOException {
 
-    if(output != null){
-        output.close();
-    }
+        if (input != null) {
+            input.close();
+        }
     }
 }
 
