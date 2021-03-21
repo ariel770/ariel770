78cde792015dc407cd738b693f0bb86f7f3e950c
Ariel Rosemberg <ariel.rosemberg@innovid.com>
2021-03-21 17:28:15 +0200

    Card shuffling and dealing with Collections method shuffle

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,61 @@
 import java.util.*;
+class Card {
+    public static enum Face {A, B, C, D, E, F, G, L, M, N, J, K, V}
 
+    public  static enum Suit {q, w, h, p}
+
+    private final Face face;
+    private final Suit suit;
+
+    public Card(Face face, Suit suit) {
+        this.face = face;
+        this.suit = suit;
+    }
+
+    public Face getFace() {
+        return face;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    @Override
+    public String toString() {
+        return face + " of " + suit;
+    }
+
+}
 public class Main {
 
+    private static List<Card> list;
+
+    public Main() {
+        Card[] card = new Card[52];
+        int count = 0;
+
+        for (Card.Suit suit : Card.Suit.values()) {
+            for (Card.Face face : Card.Face.values()) {
+                card[count] = new Card(face, suit);
+              count++;
+            }
+        }
+        list = Arrays.asList(card);
+        Collections.shuffle(list);
+
+
+    }
+
+    public static void printCards() {
+        for (int count = 0; count < list.size(); count++) {
+            System.out.printf("%-19s %s", list.get(count), (((count + 1) % 4 == 0) ? "\n" : " "));
+        }
+    }
+
+
     public static void main(String[] args) {
-        String colors[] = {"RED", "YELLOW", "BLACK", "BLUE", "WHITE"};
-       List <Time2> b = new ArrayList<>();
-         b.add(new Time2(6,6,6));
-         b.add(new Time2(12,13,13));
-         b.add(new Time2(18,5,50));
-         b.add(new Time2(6,6,6));
-         b.add(new Time2(12,13,13));
-         b.add(new Time2(18,5,50));
-        System.out.println(b);
-
-        Collections.sort(b,new TimeComparator());
-       System.out.println(b);
-
+        Main mm = new Main();
+        mm.printCards();
 
     }
 }
