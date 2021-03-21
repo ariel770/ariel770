478331d4b581ded05900b5264b09e8077d8246d2
Ariel Rosemberg <ariel.rosemberg@innovid.com>
2021-03-21 14:47:12 +0200

    Manipulating lists with List, LinkedList and ListIterator

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
@@ -11,31 +8,46 @@ public class Main {
         for (String color : colors) {
             listOne.add(color);
         }
-        String colors1[] = {"BLACK", "BLUE", "WHITE"};
-        List<String> removeList = new ArrayList<>();
-        for (String color : colors1) {
-            removeList.add(color);
-        }
-        for (int count = 0; count < listOne.size(); count++)
-            System.out.print(listOne.get(count) + " - ");
 
-        removeColors(listOne,removeList);
-        System.out.println("after removing all the duplicates : ");
-
-        for (int count = 0; count < listOne.size(); count++)
-            System.out.print(listOne.get(count) + " - ");
-
+        printList(listOne);
+        System.out.println();
+        transfromInUc(listOne);
+        printList(listOne);
+        System.out.println();
+        removeList(listOne, 1, 3);
+        printList(listOne);
+        System.out.println();
+        printReverse(listOne);
 
     }
 
-    private static void removeColors(Collection<String> listOne, Collection<String> removeList) {
-        Iterator<String> iterator = listOne.iterator();
+    private static void printReverse(List<String> listOne) {
+        ListIterator listIterator = listOne.listIterator(listOne.size());
+        while(listIterator.hasPrevious()){
+            System.out.print(listIterator.previous()+" ");
+        }
+    }
+
+    private static void transfromInUc(List<String> listOne) {
+      ListIterator<String> iterator = listOne.listIterator();
         while (iterator.hasNext()) {
-            if (removeList.contains(iterator.next())) {
-                iterator.remove();
-            }
+
+            String color = iterator.next();
+            iterator.set(color.toLowerCase());
+
         }
 
+    }
+
+    private static void removeList(List<String> listOne, int from, int to) {
+         listOne.subList(from,to).clear();
+    }
+
+    private static void printList(List<String> listOne) {
+
+        for (String colors : listOne) {
+            System.out.print(colors + " ");
+        }
     }
 }
 
