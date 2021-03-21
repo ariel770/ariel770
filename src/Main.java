8bd4eec07f0666f71bb880a1f61d7a35452e42bf
Ariel Rosemberg <ariel.rosemberg@innovid.com>
2021-03-21 12:21:17 +0200

    Manipulating lists with List, ArrayList, Collection and Iterator

--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,41 @@
-import java.io.EOFException;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
 
-public class Main extends Object {
-    private static ObjectInputStream input;
+public class Main {
 
-    public static void main(String[] args) throws IOException {
-        openFile();
-        readFile();
-        closeFile();
-
-    }
-
-    public static void openFile() {
-        try {
-            input = new ObjectInputStream(Files.newInputStream(Paths.get("./src/text.txt")));
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.out.println("error to create input stream");
+    public static void main(String[] args) {
+        String colors[] = {"RED", "YELLOW", "BLACK", "BLUE", "WHITE"};
+        List<String> listOne = new ArrayList<>();
+        for (String color : colors) {
+            listOne.add(color);
         }
+        String colors1[] = {"BLACK", "BLUE", "WHITE"};
+        List<String> removeList = new ArrayList<>();
+        for (String color : colors1) {
+            removeList.add(color);
+        }
+        for (int count = 0; count < listOne.size(); count++)
+            System.out.print(listOne.get(count) + " - ");
+
+        removeColors(listOne,removeList);
+        System.out.println("after removing all the duplicates : ");
+
+        for (int count = 0; count < listOne.size(); count++)
+            System.out.print(listOne.get(count) + " - ");
+
 
     }
 
-    public static void readFile() {
-
-        try {
-            while (true) {
-
-                AccountToSerializable records = (AccountToSerializable) input.readObject();
-                System.out.printf("%s %s %s %s %n",records.getAccountNumber(), records.getFirstName(), records.getLastName(), records.getBalance());
+    private static void removeColors(Collection<String> listOne, Collection<String> removeList) {
+        Iterator<String> iterator = listOne.iterator();
+        while (iterator.hasNext()) {
+            if (removeList.contains(iterator.next())) {
+                iterator.remove();
             }
-        } catch (EOFException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
 
-    }
-
-    public static void closeFile() throws IOException {
-
-        if (input != null) {
-            input.close();
-        }
     }
 }
 
