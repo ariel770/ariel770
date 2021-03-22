44e26057d5dba9d47fb5da0830175135e3d2c5aa
Ariel Rosemberg <ariel.rosemberg@innovid.com>
2021-03-22 10:02:22 +0200

    binary serach

--- src/Main.java
+++ src/Main.java
@@ -1,62 +1,29 @@
 import java.util.*;
-class Card {
-    public static enum Face {A, B, C, D, E, F, G, L, M, N, J, K, V}
 
-    public  static enum Suit {q, w, h, p}
-
-    private final Face face;
-    private final Suit suit;
-
-    public Card(Face face, Suit suit) {
-        this.face = face;
-        this.suit = suit;
-    }
-
-    public Face getFace() {
-        return face;
-    }
-
-    public Suit getSuit() {
-        return suit;
-    }
-
-    @Override
-    public String toString() {
-        return face + " of " + suit;
-    }
-
-}
 public class Main {
 
-    private static List<Card> list;
-
-    public Main() {
-        Card[] card = new Card[52];
-        int count = 0;
-
-        for (Card.Suit suit : Card.Suit.values()) {
-            for (Card.Face face : Card.Face.values()) {
-                card[count] = new Card(face, suit);
-              count++;
-            }
-        }
-        list = Arrays.asList(card);
-        Collections.shuffle(list);
-
-
-    }
-
-    public static void printCards() {
-        for (int count = 0; count < list.size(); count++) {
-            System.out.printf("%-19s %s", list.get(count), (((count + 1) % 4 == 0) ? "\n" : " "));
-        }
-    }
-
-
     public static void main(String[] args) {
-        Main mm = new Main();
-        mm.printCards();
+        String a[] = {"red", "blue", "green", "black"};
+        List<String> stringList = new ArrayList<>(Arrays.asList(a));
+        Collections.sort(stringList);
+        for (int count = 0; count < stringList.size(); count++) {
+            System.out.println(stringList.get(count));
+        }
+        searchIndex(stringList, "red");
+        searchIndex(stringList, "green");
+        searchIndex(stringList, "yellow");
 
+    }
+
+    public static void searchIndex(List<String> list, String key) {
+
+        int result = 0;
+        result = Collections.binarySearch(list, key);
+        if (result >= 0) {
+            System.out.println(key + " : " + result);
+        } else {
+            System.out.println(" not found " + key + "  : " + result);
+        }
     }
 }
 
