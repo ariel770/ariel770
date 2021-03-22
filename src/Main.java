2daaeb784ad4f90b875a41dbf3284272751f984f
Ariel Rosemberg <ariel.rosemberg@innovid.com>
2021-03-22 12:49:09 +0200

    Using a Map to count the number of occurrences of each word in a String

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,33 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        String a[] = {"red", "blue", "green", "black","green"};
-        List<String> stringList = new ArrayList<>(Arrays.asList(a));
-        serachIntTreeSet(stringList);
-
+        Map<String, Integer> stringMap = new HashMap<String, Integer>();
+        createMap(stringMap);
+        displayMap(stringMap);
     }
 
-    private static void serachIntTreeSet(Collection<String> stringList) {
-        SortedSet<String> set = new TreeSet<>(stringList);
+    private static void createMap(Map<String, Integer> stringMap) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter words : ");
+        String line = input.nextLine();
+        String[] words = line.split(" ");
 
-        System.out.println(set);
+        for (String word : words) {
+            String newWord = word.toLowerCase();
+            if (stringMap.containsKey(newWord)) {
+                int count = stringMap.get(newWord);
+                stringMap.put(newWord, count + 1);
+            } else {
+                stringMap.put(newWord, 1);
+            }
+        }
     }
+
+    private static void displayMap(Map<String, Integer> stringMap) {
+        Set <String> keys = stringMap.keySet();
+        TreeSet <String> treeSet = new TreeSet<>(keys);
+       for (String tree : treeSet)
+        System.out.printf("%s %s %n",tree ,stringMap.get(tree) );
+      }
 }
 
