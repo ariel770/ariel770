9646759b77486b29f13e07093b11e016d0edfdbe
Ariel Rosemberg <ariel.rosemberg@innovid.com>
2021-04-08 09:51:12 +0300

    Producer/Consumer Relationship: ArrayBlockingQueue

--- src/UnsyncronizeBuffer.java
+++ src/UnsyncronizeBuffer.java
@@ -1,18 +1,21 @@
+import java.util.concurrent.ArrayBlockingQueue;
+
 public class UnsyncronizeBuffer implements Buffer {
-    public int value = -1;
+    public ArrayBlockingQueue <Integer> buffer ;
+
+    public UnsyncronizeBuffer() {
+        buffer = new ArrayBlockingQueue<>(1);
+    }
 
     @Override
     public int consume() throws InterruptedException {
-       System.out.printf("%n Consumer read : %d %n",value);
-
-        return value;
+       int size = buffer.take();
+        return size;
     }
 
 
     @Override
-    public void produce(int value) throws InterruptedException {
-        System.out.printf("%n Producer write : %d %n",value);
-        this.value = value;
-
+    public void produce(int records) throws InterruptedException {
+        buffer.put(records);
     }
 }
